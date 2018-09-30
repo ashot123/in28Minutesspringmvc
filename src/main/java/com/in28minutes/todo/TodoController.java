430a63f104ddb8ae902b3e5852adfc4a0ac003e0
Ashot <ashot2005@yahoo.com>
2018-09-30 20:04:07 +0400

    Step 21 : Session vs Model vs Request - @SessionAttributes http://localhost:8080/login

--- src/main/java/com/in28minutes/todo/TodoController.java
+++ src/main/java/com/in28minutes/todo/TodoController.java
@@ -7,23 +7,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.in28minutes.todo.service.TodoService;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
 @Controller
+@SessionAttributes("name")
 public class TodoController {
-
-/*    private TodoService service;
-
-    @Autowired
-    public TodoController(TodoService service) {
-        this.service = service;
-    }*/
 
     @Autowired
     private TodoService service;
 
     @RequestMapping(value = "/list-todos", method = RequestMethod.GET)
-    public String showLoginPage(ModelMap model) {
-        model.addAttribute("todos", service.retrieveTodos("in28Minutes"));
+    public String showLoginPage(ModelMap model, String name) {
+        String user = (String) model.get("name");
+        model.addAttribute("todos", service.retrieveTodos(user));
         return "list-todos";
     }
 }
--- src/main/web/WEB-INF/views/login.jsp
+++ src/main/web/WEB-INF/views/login.jsp
@@ -2,7 +2,6 @@
 <head>
     <title>Yahoo!!</title>
 </head>
-
 <body>
 <p><font color="red">${errorMessage}</font></p>
 <form action="/login" method="POST">
