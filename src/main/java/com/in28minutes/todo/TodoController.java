f5345a34bf7873872996b541914228206b713c69
Ashot <ashot2005@yahoo.com>
2018-10-09 14:26:27 +0400

    Step 31 : Let’s prepare for Spring Security http://localhost:8080/

--- src/main/java/com/in28minutes/todo/TodoController.java
+++ src/main/java/com/in28minutes/todo/TodoController.java
@@ -36,7 +36,7 @@ public class TodoController {
 
     @RequestMapping(value = "/list-todos", method = RequestMethod.GET)
     public String showTodosList(ModelMap model) {
-        String user = (String) model.get("name");
+        String user = getLoggedInUserName(model);
         model.addAttribute("todos", service.retrieveTodos(user));
         return "list-todos";
     }
@@ -53,10 +53,14 @@ public class TodoController {
         if (result.hasErrors())
             return "todo";
 
-        service.addTodo((String) model.get("name"), todo.getDesc(),
+        service.addTodo(getLoggedInUserName(model), todo.getDesc(),
                 todo.getTargetDate(), false);
         model.clear();// to prevent request parameter "name" to be passed
         return "redirect:/list-todos";
+    }
+
+    private String getLoggedInUserName(ModelMap model) {
+        return (String) model.get("name");
     }
 
     @RequestMapping(value = "/update-todo", method = RequestMethod.GET)
@@ -71,7 +75,7 @@ public class TodoController {
         if (result.hasErrors())
             return "todo";
 
-        todo.setUser("in28Minutes"); //TODO:Remove Hardcoding Later
+        todo.setUser(getLoggedInUserName(model));
         service.updateTodo(todo);
 
         model.clear();// to prevent request parameter "name" to be passed
